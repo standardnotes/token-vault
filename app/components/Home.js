2a6196b0a69cf07d1539f8f51f3114cc80615000
Johnny Almonte <johnny243@users.noreply.github.com>
2021-05-25 23:30:23 -0400

    feat: reorder button

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,6 +5,7 @@ import ViewEntries from '@Components/ViewEntries';
 import ConfirmDialog from '@Components/ConfirmDialog';
 import DataErrorAlert from '@Components/DataErrorAlert';
 import EditorKit from '@standardnotes/editor-kit';
+import ReorderIcon from '@Components/ReorderIcon';
 
 const initialState = {
   text: '',
@@ -13,6 +14,7 @@ const initialState = {
   editMode: false,
   editEntry: null,
   confirmRemove: false,
+  confirmReorder: false,
   displayCopy: false,
   canEdit: true
 };
@@ -149,6 +151,7 @@ export default class Home extends React.Component {
   onCancel = () => {
     this.setState({
       confirmRemove: false,
+      confirmReorder: false,
       editMode: false,
       editEntry: null
     });
@@ -192,14 +195,38 @@ export default class Home extends React.Component {
   };
 
   updateEntries = (entries) => {
+    this.saveNote(entries);
     this.setState({
       entries
     });
   };
 
+  onReorderEntries = () => {
+    if (!this.state.canEdit) {
+      return;
+    }
+    this.setState({
+      confirmReorder: true
+    });
+  };
+
+  reorderEntries = () => {
+    const { entries } = this.state;
+    const orderedEntries = entries.sort((a, b) => {
+      const serviceA = a.service.toLowerCase();
+      const serviceB = b.service.toLowerCase();
+      return (serviceA < serviceB) ? -1 : (serviceA > serviceB) ? 1 : 0;
+    });
+    this.saveNote(orderedEntries);
+    this.setState({
+      entries: orderedEntries,
+      confirmReorder: false
+    });
+  };
+
   render() {
     const editEntry = this.state.editEntry || {};
-    const { canEdit, displayCopy, parseError, editMode, entries, confirmRemove } = this.state;
+    const { canEdit, displayCopy, parseError, editMode, entries, confirmRemove, confirmReorder } = this.state;
 
     return (
       <div className="sn-component">
@@ -217,6 +244,9 @@ export default class Home extends React.Component {
         {parseError && <DataErrorAlert />}
         <div id="header" className={!canEdit ? 'hidden' : '' }>
           <div className="sk-button-group">
+            <div onClick={this.onReorderEntries} className="sk-button info" aria-disabled={!canEdit}>
+              <ReorderIcon />
+            </div>
             <div onClick={this.onAddNew} className="sk-button info" aria-disabled={!canEdit}>
               <div className="sk-label">Add New</div>
             </div>
@@ -246,6 +276,14 @@ export default class Home extends React.Component {
               title={`Remove ${editEntry.entry.service}`}
               message="Are you sure you want to remove this entry?"
               onConfirm={() => this.removeEntry(editEntry.id)}
+              onCancel={this.onCancel}
+            />
+          )}
+          {confirmReorder && (
+            <ConfirmDialog
+              title={'Reorder entries'}
+              message="Are you sure you want to reorder entries?"
+              onConfirm={this.reorderEntries}
               onCancel={this.onCancel}
             />
           )}
